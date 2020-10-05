a646ae1656b0beb8084e4a20c1171da0b0a14d15
Alastair Mucklow <amucklow@strangerpixel.com>
2020-10-05 16:31:11 +0100

    Add onBlur validation

--- view/frontend/web/js/internationalTelephoneInput.js
+++ view/frontend/web/js/internationalTelephoneInput.js
@@ -2,8 +2,42 @@ define([
     'jquery',
     'intlTelInput'
 ], function ($) {
-    var initIntl = function (config, node) {
-        $(node).intlTelInput(config);
+
+    return function (config, node) {
+
+        var input = $(node),
+            errorMsg = $("#error-msg"),
+            validMsg = $("#valid-msg");
+
+        // here, the index maps to the error code returned from getValidationError - see readme
+        var errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
+
+        // initialise plugin
+        var iti = input.intlTelInput(config);
+
+        var reset = function () {
+            input.removeClass("mage-error");
+            errorMsg.html("");
+            errorMsg.addClass("hide");
+            validMsg.addClass("hide");
+        };
+
+        // on blur: validate
+        input.on('blur', function () {
+            reset();
+            if (input.val().trim()) {
+                if ($(this).intlTelInput("isValidNumber")) {
+                    validMsg.removeClass("hide");
+                } else {
+                    input.addClass("mage-error");
+                    var errorCode = $(this).intlTelInput("getValidationError");
+                    errorMsg.html(errorMap[errorCode]);
+                    errorMsg.removeClass("hide");
+                }
+            }
+        });
+
+        // on keyup / change flag: reset
+        input.on('change keyup', reset);
     };
-    return initIntl;
 });
